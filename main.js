431500466b04d17132a077eea45b1f77343e2ecc
abdullodev <abdulloergashxojayev01@gmail.com>
2023-09-04 15:50:01 +0500

    ok

---
+++ main.js
@@ -0,0 +1,20 @@
+function onTime() {
+  const min = document.getElementById("min");
+  const secund = document.getElementById("secund");
+  const hour = document.getElementById("hour");
+  let date = new Date();
+  let s = date.getSeconds();
+  let m = date.getMinutes();
+  let h = date.getHours();
+
+  secund.style.transform = `rotate(${s * 6}deg)`;
+  min.style.transform = `rotate(${m * 6}deg)`;
+  hour.style.transform = `rotate(${h * 30 + m / 2}deg)`;
+
+  let sound = new Audio("sound.mp3");
+  sound.play();
+}
+
+onTime();
+
+setInterval(onTime, 1000);
